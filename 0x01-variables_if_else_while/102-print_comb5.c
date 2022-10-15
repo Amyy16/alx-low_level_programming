59aa2839aeb6fd5aab51b143614ede3b7b705754
Amarachi <nwabueze2amarachi@gmail.com>
2022-10-15 16:37:20 -0700

    variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Prints two digits
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			putchar(e);
+			putchar(i);
+			if (i == 57 && e == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
+
